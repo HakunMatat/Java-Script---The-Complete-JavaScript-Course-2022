4bb4a6e2edac4dd585e52b8af661c26f567f7b20
Dimitar Sabev <97794162+HakunMatat@users.noreply.github.com>
2022-10-08 23:26:53 +0300

    commit

--- 01-Fundamentals-Part-1/03_challenges.js
+++ 01-Fundamentals-Part-1/03_challenges.js
@@ -89,8 +89,3 @@ const tip = bill >= 50 && bill <= 300 ? bill * 15/100 : bill * 20/100
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was ${bill + tip}.`);
 */
 //
-
-
-
-
-
