316c8f3410e5c887a1d298c79ebdd7689297683c
Dimitar Sabev <97794162+HakunMatat@users.noreply.github.com>
2022-10-08 21:25:04 +0300

    commit

--- 03-Developer-Skills/01_script.js
+++ 03-Developer-Skills/01_script.js
@@ -1,5 +1,6 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict'
+
 /*
 const x = '123'
 
@@ -8,3 +9,25 @@ console.log()
 console.log()
 */
 //
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+
+    // C) FIX THE BUG
+    value: Number(prompt('Dregrees celsius:')),
+  }
+
+  // B) FIND THE BUG
+  console.table(measurement)
+
+  //   console.warn(measureKelvin.value)
+  //   console.error(measureKelvin.value)
+
+  const kelvin = measurement.value + 273
+  return kelvin
+}
+
+// A) IDENTIFY THE BUG
+console.log(measureKelvin())
