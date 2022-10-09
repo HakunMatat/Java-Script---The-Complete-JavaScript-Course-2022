ef7889cb67b8342306c5eb00ea036ea802190897
Dimitar Sabev <97794162+HakunMatat@users.noreply.github.com>
2022-10-09 16:29:07 +0300

    commit

--- 03-Developer-Skills/01_script.js
+++ 03-Developer-Skills/01_script.js
@@ -51,29 +51,48 @@ console.log()
 //
 
 const calcTempAmplitudeBug = (bb, eeb) => {
-  const temps = bb.concat(eeb);
+	const temps = bb.concat(eeb);
 
-  let max = temps[0];
-  let min = temps[0];
-  console.log(max);
+	let max = temps[0];
+	let min = temps[0];
+	console.log(max);
 
-  for (let i = 0; i < temps.length; i++) {
-    let curTemp = temps[i];
+	for (let i = 0; i < temps.length; i++) {
+		let curTemp = temps[i];
 
-    if (typeof curTemp !== 'number') continue;
+		if (typeof curTemp !== 'number') continue;
 
-    if (curTemp > max) {
-      max = curTemp;
-    }
-    if (curTemp < min) {
-      min = curTemp;
-    }
-  }
-  console.log(min, max);
-  return max - min;
+		if (curTemp > max) {
+			max = curTemp;
+		}
+		if (curTemp < min) {
+			min = curTemp;
+		}
+	}
+	console.log(min, max);
+	return max - min;
 };
 
 const amplitudeBug = calcTempAmplitudeBug([3, 5, 1, 'error'], [9, 4, 5]);
 console.log(amplitudeBug);
 */
 //
+
+// Data 1
+// const arr = [17, 21, 23];
+// Data 2
+const arr = [12, 5, -5, 0, 4];
+
+const days = arr.length;
+
+const printForecast = function (temp, days) {
+    for (let i = 0; i < arr.length; i++) {
+        const numberOfDays = arr.indexOf(arr[i]);
+        console.log(`${arr[i]}ºC in ${numberOfDays + 1} days`);
+    }
+};
+
+printForecast();
+
+// Problem one: We need to make the days equal to the tempetures lenghts
+// Problem two: We need to make a loop so it can count 1,2,3.. and print everything on one line
