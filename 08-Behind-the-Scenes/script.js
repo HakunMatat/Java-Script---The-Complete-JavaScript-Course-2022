d9d3f98691c16bee0d4d8d6e2d1de8fcf1fae50d
Dimitar Sabev <97794162+HakunMatat@users.noreply.github.com>
2022-10-21 18:29:14 +0300

    commit

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -34,10 +34,44 @@ calcAge(1991);
 */
 //
 
+// Hoisting with Variables
 console.log(me);
-console.log(job);
-console.log(year);
+// console.log(job);
+// console.log(year);
 
 var me = 'Jonas';
 let job = 'teacher';
 const year = 1991;
+
+// Hoisting with Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+    return a + b;
+}
+// underfined is not a function
+var addExpr = function (a, b) {
+    return a + b;
+};
+// underfined is not a function
+var addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
