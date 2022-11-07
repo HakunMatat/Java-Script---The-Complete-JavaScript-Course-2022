2855e012d0ddce7da7a02f44b980b7bfe7807393
Dimitar Sabev <97794162+HakunMatat@users.noreply.github.com>
2022-11-08 01:55:53 +0200

    reverse

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -206,5 +206,5 @@ jessicaCopy.lastName = 'Davis';
 jessicaCopy.family.push('Mary');
 jessicaCopy.family.push('John');
 console.log('Before marriage:', jessica2);
-console.log('After marriage:', jessicaCopy);
+// console.log('After marriage:', jessicaCopy);
 */
